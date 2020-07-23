d8e5fd913da6189f13fa52ff234d72f20764971a
Alexandru Scurtu <sasha.scurtu@gmail.com>
2020-07-23 17:04:11 +0530

    raphael: ShapeShift-ify
    Signed-off-by: Alexandru Scurtu <sasha.scurtu@gmail.com>

--- aosp_raphael.mk
+++ aosp_raphael.mk
@@ -1,20 +1,31 @@
 #
-# Copyright (C) 2019 The LineageOS Project
+# Copyright (C) 2019 The AOSP Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# Inherit from Raphael
 $(call inherit-product, device/xiaomi/raphael/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common AOSP stuff.
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions.
 PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := raphael
 PRODUCT_MANUFACTURER := Xiaomi
-PRODUCT_NAME := lineage_raphael
+PRODUCT_NAME := aosp_raphael
+
+#GAPPS
+TARGET_GAPPS_ARCH = arm64
+
+#BOOTANIMATION
+TARGET_BOOT_ANIMATION_RES = 1080
 
 BUILD_FINGERPRINT := "Xiaomi/raphaelin/raphaelin:9/PKQ1.181121.001/V10.3.3.0.PFKINXM:user/release-keys"
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+#Shapeshift stuff
+CUSTOM_BUILD_TYPE := OFFICIAL
+TARGET_FACE_UNLOCK_SUPPORTED := true
