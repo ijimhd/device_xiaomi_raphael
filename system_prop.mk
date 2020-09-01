a24024ab96e522afed1446c200b06071271709b2
dianlujitao <dianlujitao@lineageos.org>
2020-09-01 20:02:44 +0300

    raphael: Force enable mobile network settings v2
     * AOSP defaults to true but 10 QC telephony extension sets it to false.
     * Restore AOSP behavior by enabling it with a system property which has
       higher priority over global system settings which is used by QCOM w/o
       modifying the jar.
    
    Change-Id: I45854b91bb8cfafabfe811777cda9f1e1840febf
    Signed-off-by: UtsavisGreat <utsavbalar1231@gmail.com>
    Signed-off-by: Joey Huab <joey@evolution-x.org>

--- system_prop.mk
+++ system_prop.mk
@@ -79,6 +79,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.data.iwlan=1 \
     persist.data.iwlan.ipsec.ap=1 \
     persist.sys.cust.lte_config=true \
+    persist.sys.fflag.override.settings_network_and_internet_v2=true \
     persist.dbg.volte_avail_ovr=1 \
     persist.dbg.vt_avail_ovr=1 \
     persist.dbg.wfc_avail_ovr=1 \
